2cc8c2b24ecfb977affe958380f6aabd784ce3cd
Thiago099 <thiago.kaique01@gmail.com>
2022-04-29 10:19:36 -0300

    [refactor] identação

--- Tarefa 1/src/build_tree/build_tree.ts
+++ Tarefa 1/src/build_tree/build_tree.ts
@@ -3,10 +3,11 @@ import build_structure from "./methods/parse_tree";
 export default function build_tree(input: string): string 
 {
     if(input.trim().length == 0) throw "E4 - Invalid input";
-    
+
     const parsed = parse(input)
 
     if(parsed.some(item => item.length != 2)) throw "E4 - Invalid input"
+    
     const root = build_structure(parsed);
 
     return root?.toString() ?? "";
