22d74d4ce8e0a993227acae11514c1a28cbd5541
Thiago099 <thiago.kaique01@gmail.com>
2022-04-29 09:18:18 -0300

    [refactor] identação

--- Tarefa 1/test/build_tree.spec.ts
+++ Tarefa 1/test/build_tree.spec.ts
@@ -2,27 +2,37 @@ import { build_tree } from '../src/build_tree/build_tree';
 describe("operate.ts", () => {
 
     test("success", () => {
-        expect(build_tree("[A,B] [A,C] [B,G] [C,H] [E,F] [B,D] [C,E]")).toBe("A[[B[[G][D]]][C[[H][E[[F]]]]]]");
+        expect(build_tree("[A,B] [A,C] [B,G] [C,H] [E,F] [B,D] [C,E]"))
+            .toBe("A[[B[[G][D]]][C[[H][E[[F]]]]]]");
     })
     
     test("E1 - Mora than two children",()=>{
-        expect(() => build_tree("[A,B] [A,C] [A,D]")).toThrow("E1 - Mora than two children");
+        expect(() => build_tree("[A,B] [A,C] [A,D]"))
+            .toThrow("E1 - Mora than two children");
     })
 
     test("E2 - Cycle",()=>
     {
-        expect(() => build_tree("[A,B] [A,C] [B,D] [D,C]")).toThrow("E2 - Cycle");
+        expect(() => build_tree("[A,B] [A,C] [B,D] [D,C]"))
+            .toThrow("E2 - Cycle");
     })
 
     test("E3 - Multiple roots",()=>
     {
-        expect(() => build_tree("[A,B] [A,C] [B,D] [D,C] [C,A]")).toThrow("E3 - Multiple roots");
+        expect(() => build_tree("[A,B] [A,C] [B,D] [D,C] [C,A]"))
+            .toThrow("E3 - Multiple roots");
     })
 
     test("E4 - Invalid input",()=>{
-        expect(() => build_tree("[A,B,C]")).toThrow("E4 - Invalid input");
-        expect(() => build_tree("")).toThrow("E4 - Invalid input");
-        expect(() => build_tree("[A,B A,C]")).toThrow("E4 - Invalid input");
+
+        expect(() => build_tree("[A,B,C]"))
+            .toThrow("E4 - Invalid input");
+
+        expect(() => build_tree(""))
+            .toThrow("E4 - Invalid input");
+        
+        expect(() => build_tree("[A,B A,C]"))
+            .toThrow("E4 - Invalid input");
     })
     
 
