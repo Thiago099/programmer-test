ae7fda7257d7bcc3eebc65a8d338eb56af5e40fe
Thiago099 <thiago.kaique01@gmail.com>
2022-04-29 09:33:39 -0300

    [fix] index

--- Tarefa 1/src/index.ts
+++ Tarefa 1/src/index.ts
@@ -1,5 +1,11 @@
-import { build_tree } from './build_tree/build_tree';
+import build_tree from './build_tree/build_tree';
 
-console.log(
-    build_tree("[A,B] [A,C] [B,D] [D,C]")
-)
+try{
+    console.log(
+        build_tree("[A,B] [A,C] [B,D] [D,C]")
+    )
+}
+catch(e)
+{
+    console.log(e);
+}
