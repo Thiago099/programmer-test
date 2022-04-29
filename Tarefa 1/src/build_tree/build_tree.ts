9d9b12d40fb9884c9e06a53a70b6f29b5729ea14
Thiago099 <thiago.kaique01@gmail.com>
2022-04-29 08:54:54 -0300

    [refactor] variable name

--- Tarefa 1/src/build_tree/build_tree.ts
+++ Tarefa 1/src/build_tree/build_tree.ts
@@ -1,7 +1,7 @@
 import parse from './methods/parse_tree_string'
 import build_structure from "./methods/parse_tree";
-export function build_tree(inputs:string): string 
+export function build_tree(input: string): string 
 {
-    const root = build_structure(parse(inputs));
+    const root = build_structure(parse(input));
     return root?.toString() ?? "";
 }
