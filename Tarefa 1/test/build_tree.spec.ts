1434815f19b29403899ec71cb46f41a6dea3c344
Thiago099 <thiago.kaique01@gmail.com>
2022-04-29 09:15:27 -0300

    [refactor] sort

--- Tarefa 1/test/build_tree.spec.ts
+++ Tarefa 1/test/build_tree.spec.ts
@@ -5,6 +5,10 @@ describe("operate.ts", () => {
         expect(build_tree("[A,B] [A,C] [B,G] [C,H] [E,F] [B,D] [C,E]")).toBe("A[[B[[G][D]]][C[[H][E[[F]]]]]]");
     })
     
+    test("E1 - Mora than two children",()=>{
+        expect(() => build_tree("[A,B] [A,C] [A,D]")).toThrow("E1 - Mora than two children");
+    })
+
     test("E2 - Cycle",()=>
     {
         expect(() => build_tree("[A,B] [A,C] [B,D] [D,C]")).toThrow("E2 - Cycle");
@@ -13,10 +17,6 @@ describe("operate.ts", () => {
     test("E3 - Multiple roots",()=>
     {
         expect(() => build_tree("[A,B] [A,C] [B,D] [D,C] [C,A]")).toThrow("E3 - Multiple roots");
-    })
-
-    test("E1 - Mora than two children",()=>{
-        expect(() => build_tree("[A,B] [A,C] [A,D]")).toThrow("E1 - Mora than two children");
     })
 
     test("E4 - Invalid input",()=>{
