aa0a873cc84b0bd64f874362917e8509455c0ac8
Thiago099 <thiago.kaique01@gmail.com>
2022-04-29 09:52:01 -0300

    [fix] identation

--- Tarefa 2/resposta.sql
+++ Tarefa 2/resposta.sql
@@ -1,17 +1,17 @@
 WITH best_salaries AS 
 (
-	SELECT	    id_departament, 
+	SELECT	 id_departament, 
 			    MAX(person.salary) salary
-	FROM 		person
-	GROUP BY    id_departament
+	FROM 		 person
+	GROUP BY  id_departament
 )
 SELECT 		departament.name departament,
             GROUP_CONCAT(person.name ORDER BY person.name ASC SEPARATOR ', ') person,
             person.salary
-FROM 		person 
+FROM 			person 
 INNER JOIN 	best_salaries 
 ON 			best_salaries.salary = person.salary 
-AND 		best_salaries.id_departament = person.id_departament
+AND 			best_salaries.id_departament = person.id_departament
 INNER JOIN 	departament 
 ON 			departament.id = person.id_departament
 GROUP BY 	departament.id
